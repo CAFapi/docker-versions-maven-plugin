9d82aa323c19ccb0bef61ef07a6b00e20e741ec1
kusumaghoshdastidar <kusumaghoshdastidar@gmail.com>
2024-05-23 18:48:49 -0700

    Sanitize the project docker registry name

--- docker-versions-maven-plugin/src/main/java/com/github/cafapi/docker_versions/plugins/DockerVersionsMojo.java
+++ docker-versions-maven-plugin/src/main/java/com/github/cafapi/docker_versions/plugins/DockerVersionsMojo.java
@@ -16,15 +16,20 @@
 package com.github.cafapi.docker_versions.plugins;
 
 import java.util.List;
+
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 import org.apache.maven.settings.Settings;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 abstract class DockerVersionsMojo extends AbstractMojo
 {
     protected static final String PROJECT_DOCKER_REGISTRY = "projectDockerRegistry";
     protected static final String LATEST_TAG = "latest";
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DockerVersionsMojo.class);
 
     @Parameter(defaultValue = "${project}", readonly = true, required = true)
     protected MavenProject project;
@@ -46,8 +51,26 @@ abstract class DockerVersionsMojo extends AbstractMojo
 
     protected String getProjectDockerRegister()
     {
-        return project.getProperties().getProperty(
+        final String projectDockerReg = project.getProperties().getProperty(
             PROJECT_DOCKER_REGISTRY,
             project.getArtifactId() + "-" + project.getVersion() + ".project-registries.local");
+
+        final String sanitizedProjectDockerReg = sanitize(projectDockerReg);
+
+        project.getProperties().put(PROJECT_DOCKER_REGISTRY, sanitizedProjectDockerReg);
+
+        return sanitizedProjectDockerReg;
+    }
+
+    private static String sanitize(final String projectDockerReg)
+    {
+        // Valid characters are case insensitive alphabets (a-z) (A-Z), digits (0-9), minus sign (-), and period (.)
+        // replace all other chars with 2 hyphens
+        if (!projectDockerReg.matches("[a-zA-Z0-9-.]+")) {
+            final String sanitizedProjectDockerReg = projectDockerReg.replaceAll("[^a-zA-Z0-9-.]+", "--");
+            LOGGER.warn("Sanitized project docker registry from {} to {}", projectDockerReg, sanitizedProjectDockerReg);
+            return sanitizedProjectDockerReg;
+        }
+        return projectDockerReg;
     }
 }
