f4f4b848557e065497988b4b02263c4a20fdb633
kusumaghoshdastidar <kusumaghoshdastidar@gmail.com>
2024-08-14 14:44:32 -0700

    Remove import

--- docker-versions-maven-plugin/src/main/java/com/github/cafapi/docker_versions/plugins/DockerVersionsUpdaterMojo.java
+++ docker-versions-maven-plugin/src/main/java/com/github/cafapi/docker_versions/plugins/DockerVersionsUpdaterMojo.java
@@ -23,7 +23,6 @@ import java.util.Set;
 
 import javax.xml.stream.XMLStreamException;
 
-import org.apache.maven.execution.MavenSession;
 import org.apache.maven.model.Plugin;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
